a78ec1792864f6cbf94e7333401f01736b65798b
oops <haikrivarb@gmail.com>
2026-03-30 22:36:03 +0530

    Update PasswordChecker code

--- PasswordChecker.java
+++ PasswordChecker.java
@@ -12,32 +12,21 @@ public class PasswordChecker {
 
         int score = 0;
 
-        // Rule 1: Minimum Length
         if (password.length() >= 8) {
             score++;
         }
-
-        // Rule 2: Contains Uppercase Letter
         if (password.matches(".*[A-Z].*")) {
             score++;
         }
-
-        // Rule 3: Contains Lowercase Letter
         if (password.matches(".*[a-z].*")) {
             score++;
         }
-
-        // Rule 4: Contains Number
         if (password.matches(".*[0-9].*")) {
             score++;
         }
-
-        // Rule 5: Contains Special Character
         if (password.matches(".*[@#$%^&+=].*")) {
             score++;
         }
-
-        // Result
         System.out.println("\nPassword Analysis Result:");
 
         if (score <= 2) {
